500aa80a7b57469e9011ae8cae6e94e5e93c499d
Clash2453 <spasmilenkov123@gmail.com>
2022-08-31 14:22:50 +0300

    Create the navbar element

---
+++ src/components/Navigation.vue
@@ -0,0 +1,51 @@
+<!-- <script setup>
+
+</script>
+ -->
+
+<template>
+    <nav class="navigation">
+        <ul class="container">
+            <li><a class="nav-link" href="#About-me">About me</a></li>
+            <li><a class="nav-link" href="#Projects">My projects</a></li>
+            <li><a class="nav-link" href="#Contacts">Contacts</a></li>
+        </ul>
+    </nav>
+
+</template>
+
+<style scoped>
+    .navigation{
+        position: fixed;
+        top: 0;
+        height: 90px;
+        width: calc(100% - 2rem);;
+        background-color:#141E1F;
+
+        display: flex;
+        align-items: center;
+        flex-direction: row-reverse;
+        padding: 0 1rem;
+    }
+    .nav-link{
+        color: #CAA387;
+        font-family: 'Roboto', sans-serif;
+        text-decoration: none;
+        font-size: 25pt;
+
+        padding: 0 0.25rem;
+    }
+
+    .nav-link:hover{
+        background-color:#283d3f;
+        text-decoration: underline;
+    }
+
+    .container{
+        display: flex;
+        flex-direction: row-reverse;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 2rem;
+    }
+</style>
