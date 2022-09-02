122b14bc88ee803aa965e5e06273b4489893dbdd
Clash2453 <spasmilenkov123@gmail.com>
2022-09-02 12:09:08 +0300

    Refactor the welcome section component

--- src/components/WelcomeSection.vue
+++ src/components/WelcomeSection.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="main-container">
-        <h1 class="greeting">Lorem Ipsum</h1>
+        <h1 class="greeting">Imagination is the limit, so lets imagine together.</h1>
     </div>
 </template>
 
@@ -9,12 +9,14 @@
         max-width: 100%;
         height: calc(100vh - 90px);
         margin-top: 90px;
-        background: url(./images/main-banner.jpeg);
+        background: url(public/images/main-banner.jpeg), linear-gradient(0deg, rgba(0,0,0,1) 10%, rgba(89,125,140,0) 20%);
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
+        /* background-color: rgba(0, 0, 0, 0.2); */
+        background-blend-mode: darken;
         display: flex;
-        flex-direction: column;
+        flex-direction: column-reverse;
         align-items: center;
         padding-top: 1.5rem;
     }
@@ -22,12 +24,17 @@
     .greeting{
         color: #CAA387;
         font-family: 'Roboto', sans-serif;
-        font-size: 3.5rem;
+        font-size: 3rem;
         z-index: 2;
+        padding-bottom: 5rem;
+        line-break: auto;
+        widows: 90%;
+        text-align: center;
     }
     @media only screen and (max-width: 720px){
         .greeting{
-            font-size: 2rem;
+            font-size: 1.5rem;
+            padding-bottom: 0;
         }
         #main-container{
             height: calc(290px -  1rem);
