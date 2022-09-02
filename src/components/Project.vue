0cc98a915e52159c7f255dc33cacb437b8646065
Clash2453 <spasmilenkov123@gmail.com>
2022-09-02 22:59:00 +0300

    Populate links

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -7,8 +7,12 @@
             <h1 class="wrapper-title">Tech Stack:</h1>
             <h2 v-for="(language, i) in project.software" class="language-title" :key="i">{{language}}</h2>
             <div class="link-wrapper">
-                <a href="">project.</a>
-                <a href=""></a>
+                <a class="link" :href="project.githubRepo">Github repo</a>
+                <a 
+                class="link" 
+                :href="project.livePreview"
+                v-if="project.livePreview ? false : true"
+                >Life Preview</a>
             </div>
         </div>
     </div>
@@ -28,8 +32,8 @@
 <style scoped>
     .project-card{
         width: 20%;
-        min-width: 300px;
-        height: 500px;
+        min-width:calc(300px - 2rem) ;
+        height: 750px;
 
         display: flex;
         flex-direction: column;
@@ -50,7 +54,6 @@
         border-radius: 10px;
     }
     .project-card:hover{
-        transform: scale(1.01);
         background-color: rgba(255, 222, 224, 0.2);
     }
     .project-title{
@@ -61,17 +64,49 @@
     .wrapper-title{
         border-radius: 5px;
         text-align: center;
-        background-color: rgba(255, 222, 224, 0.2);
         width: 90%;
         align-self: center;
     }
 
     .wrapper{
         display: flex;
-        flex-direction: column;
+        flex-wrap: wrap;
+        align-items:center ;
+        justify-content: center;
         gap: 1rem;
         width: 100%;
+        height: 500px;
         border-radius: 5px;
-        padding: 0.5rem;
+        padding: 1rem 0.5rem;
+        background-color: rgba(255, 222, 224, 0.2);
+    }
+    .link-wrapper{
+        display: flex;
+        width: 90%;
+        align-items: center;
+        justify-content: center;
+        gap: 2rem;
+    }
+    .link{
+        height: 3rem;
+        width: 8rem;
+
+        font-size: 1rem;
+        font-weight: 700;
+        text-decoration: none;
+        background-color: rgb(0, 164, 107);
+        border-radius: 5px;
+        color: white;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .link:hover{
+        background-color: rgb(2, 187, 122);
+        color: rgb(51, 54, 63);
+    }
+    .language-title{
+        font-size: 1.5rem;
     }
 </style>
