021a7945fb7136b7cffede41e9c13bbf0c663bbe
Clash2453 <spasmilenkov123@gmail.com>
2022-09-03 23:10:25 +0300

    Add fade animation

--- src/components/WelcomeSection.vue
+++ src/components/WelcomeSection.vue
@@ -9,16 +9,17 @@
         max-width: 100%;
         height: calc(100vh - 90px);
         margin-top: 90px;
-        background: url(public/images/main-banner.jpeg), linear-gradient(0deg, rgba(0,0,0,1) 10%, rgba(89,125,140,0) 20%);
+        background: url(/images/main-banner.webp), linear-gradient(0deg, rgba(0,0,0,1) 10%, rgba(89,125,140,0) 20%);
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
-        /* background-color: rgba(0, 0, 0, 0.2); */
         background-blend-mode: darken;
         display: flex;
         flex-direction: column-reverse;
         align-items: center;
         padding-top: 1.5rem;
+        animation: fade-in;
+        animation-duration: 4s;
     }
 
     .greeting{
@@ -39,6 +40,9 @@
         #main-container{
             height: calc(290px -  1rem);
             padding-bottom: 1.5rem;
+            background: url(/images/main-banner-mobile.webp), linear-gradient(0deg, rgba(0,0,0,1) 10%, rgba(89,125,140,0) 20%);
+            background-position: center;
+            background-size: cover;
         }
     }
 </style>
