b297709be647ac0eb8e7f5a17f18e8eb0001695a
Clash2453 <spasmilenkov123@gmail.com>
2022-09-13 21:11:57 +0300

    Rework the welcome section for pc

--- src/components/WelcomeSection.vue
+++ src/components/WelcomeSection.vue
@@ -1,36 +1,55 @@
 <template>
     <div id="main-container">
-        <h1 class="greeting">Imagination is the limit, so lets imagine together.</h1>
+        <div class="container">
+            <h1 class="greeting">Hi my name's Spas</h1>
+            <p class="short-text">A passionate computer science student and aspiring frontend developer.</p>
+        </div>
+        <div class="image-container"></div>
     </div>
 </template>
 
 <style scoped>
     #main-container{
         max-width: 100%;
-        height: calc(100vh - 90px);
-        margin-top: 90px;
-        background: url(/images/main-banner.webp), linear-gradient(0deg, rgba(0,0,0,1) 10%, rgba(89,125,140,0) 20%),radial-gradient(circle, rgba(0,0,0,0) 57%, rgba(0,0,0,1) 100%);;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+    .container{
+        width: 30%;
+        height: 100%;
+        background-color: #13233B;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 25%;
+    }
+    .image-container{
+        width: 70%;
+        height: 100vh;
+        background: url("/images/mountain2.jpg");
+        background-color: rgba(0, 0, 0, 0.2);
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
         background-blend-mode: darken;
-        display: flex;
-        flex-direction: column-reverse;
-        align-items: center;
-        padding-top: 1.5rem;
-        animation: fade-in;
-        animation-duration: 4s;
     }
-
+    .short-text{
+        font-size: 1.5rem;
+        font-family: 'Lato', sans-serif;
+        color: #C0C0D8;
+        width: 80%;
+        text-align: left;
+        line-height: 2rem;
+    }
     .greeting{
         color: #CAA387;
         font-family: 'Roboto', sans-serif;
         font-size: 3rem;
         z-index: 2;
-        padding-bottom: 5rem;
         line-break: auto;
-        widows: 90%;
-        text-align: center;
+        width: 80%;
     }
     @media only screen and (max-width: 720px){
         .greeting{
