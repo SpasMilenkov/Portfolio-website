88db31fadde6ade5279730735cb2f911b9c8a885
Clash2453 <spasmilenkov123@gmail.com>
2022-09-15 22:52:59 +0300

    Remove old contacts form

--- src/components/ContactsForm.vue
+++ src/components/ContactsForm.vue
@@ -1,109 +1,59 @@
 <template>
-    <div class="wrapper" v-if="formActive" >
-        <div class="contacts-form-image" ></div>
-        <div class="contacts">
-            <div class="top-row">
-                <h1 class="section-header">Contacts</h1>
-                <i class="fi fi-br-cross icon" @click="closeForm" ></i>
-            </div>
-            <div class="link-wrapper">
-                <a class="link" href="" v-on:click.prevent="copyURL">
-                    <h1 v-if="!copied" class="icon">Copy email</h1>
-                    <h1 v-if="copied" class="icon">Copied!</h1>
-            </a>
-            <a class="link" href="https://github.com/Clash2453">
-                <i class="fi fi-brands-linkedin icon"></i>
-            </a>
-            </div>
-        </div>
+    <div class="wrapper">
+        <h1 class="section-header">Contacts</h1>
+        <label for="">
+            <input class="text-input" type="text">
+        </label>
+        <label for="">
+            <input class="text-input" type="text">
+        </label>
+        <label for="">
+            <input class="text-input" type="text">
+        </label>
+        <button class="send-button">Send email</button>
     </div>
 </template>
 
 <script>
     export default{
-        props: ['formActive'],
         data: function(){
-            return{
-                copied: false
-            }
+            return{}
         },
-        methods: {
-            closeForm() {
-                this.$parent.toggleForm();
-            },
-            async copyURL() {
-                try {
-                    await navigator.clipboard.writeText('spasmilenkov2453@gmail.com');
-                    this.copied = true;
-                } catch ($e) {
-                    console.log('Cannot copy');
-                }
-            }
-            
-        }
     }
 </script>
 
 <style scoped>
     .wrapper{
-        min-height: 600px;
-        min-width: 320px;
-        max-width: 1000px;
-        width: 95%;
-        display: flex;
-        flex-direction: row;
-        gap: 3px;
-        z-index: 100;
-        position: fixed;
-        position: fixed;
-        top: 25%;
-        left: 50%;
-        transform: translateX(-50%);
-        background-color: rgb(0, 255, 166);
-    }
-    .contacts-form-image{
-        background: url(/images/contacts-banner.webp);
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-
-        width: 50%;
-        min-height: 300px;
-    }
-    .contacts{
-        width: 50%;
-        background-color: #2A2B3A;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-between;
-    }
-    .top-row{
-        display: flex;
-        width: calc(100% - 1rem);
-        justify-content: space-between;
-        align-items: center;
-        padding: 0.5rem;
-    }
-    .link-wrapper{
+        width: 100%;
+        height: 100%;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        height: 300%;
+        gap: 3rem;
 
+        background: url('/images/the-world.jpg');
+        background-color: rgba(0, 0, 0, 0.7);
+        background-position: center;
+        background-size: cover;
+        background-blend-mode: darken;
     }
-    .icon{
+    .text-input{
+        border: none;
+        background-color: white;
+        border-radius: 5px;
+        width: 25rem;
+        height: 2rem;
+    }
+    .send-button{
+        background-color: rgb(22, 206, 142);
+        color: whitesmoke;
         font-size: 1.5rem;
-        text-decoration: none;
+        font-weight: 600;
         font-family: 'Lato', sans-serif;
-        font-display: swap;
-    }
-    .fi-br-cross{
-        padding-top: 0.5rem;
-        font-size: 2.5rem;
-        color: rgb(199, 45, 78);
+        padding: 1rem;
+        border: none;
+        border-radius: 5px;
     }
     @media only screen and (max-width: 650px){
         .wrapper{
@@ -111,10 +61,6 @@
         }
         .section-header{
             font-size: 2rem;
-        }
-        .fi-br-cross{
-            padding-top: 0.5rem;
-            font-size: 1.5rem;
         }
     }
 </style>
