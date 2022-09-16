a3795d855416255e40f4e6cbe5b98160254e6536
Clash2453 <spasmilenkov123@gmail.com>
2022-09-16 22:33:45 +0300

    Create the design for the form

--- src/components/ContactsForm.vue
+++ src/components/ContactsForm.vue
@@ -1,17 +1,22 @@
 <template>
     <div class="wrapper">
         <h1 class="section-header">Contacts</h1>
-        <label for="">
-            <input class="text-input" type="text">
-        </label>
-        <label for="">
-            <input class="text-input" type="text">
-        </label>
-        <label for="">
-            <input class="text-input" type="text">
-        </label>
-        <button class="send-button">Send email</button>
-    </div>
+        <div class="container">
+            <label class="input-container" for="sender">
+                <input class="text-input" type="text" placeholder="John Doe...">
+            </label>
+            <label class="input-container" for="">
+                <input class="text-input" type="text" placeholder="Title...">
+            </label>
+            <label class="input-container" for="">
+                <input class="text-input" type="text" placeholder="Subject...">
+            </label>
+            <label class="input-container" for="">
+                <input class="text-input" type="text" placeholder="Message...">
+            </label>
+            <button class="send-button">Send email</button>
+        </div>
+        </div>
 </template>
 
 <script>
@@ -23,30 +28,47 @@
 </script>
 
 <style scoped>
-    .wrapper{
-        width: 100%;
-        height: 100%;
+    .wrapper,
+    .container{
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         gap: 3rem;
+    }
+    .container{
+        background-color: #13151D;
+        padding: 2rem 1rem;
+        border-radius: 10px;
+    }
+    .wrapper{
+        width: 100%;
+        height: 100%;
 
         background: url('/images/the-world.jpg');
-        background-color: rgba(0, 0, 0, 0.7);
+        background-color: rgba(0, 0, 0, 0.5);
         background-position: center;
         background-size: cover;
         background-blend-mode: darken;
     }
     .text-input{
         border: none;
-        background-color: white;
-        border-radius: 5px;
+        background-color: rgba(200, 200, 200, 0.1);
+        color: rgba(210, 210, 210, 1);
+        padding-top: 0.5rem;
+        font-size: 1.5rem;
+        height: 3.5rem;
+        border-bottom: 3px solid rgb(16, 143, 99);
+        font-family: 'Robot', sans-serif;
         width: 25rem;
         height: 2rem;
     }
+    .text-input:focus{
+        outline: none;
+        background-color: rgba(200, 200, 200, 0.3);
+    }
     .send-button{
-        background-color: rgb(22, 206, 142);
+        background-color: rgb(16, 143, 99);
         color: whitesmoke;
         font-size: 1.5rem;
         font-weight: 600;
