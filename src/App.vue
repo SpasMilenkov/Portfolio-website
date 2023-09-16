64ab5eaf784adbb84fe6a7ae2c2f934c6460dcb3
Spas Milenkov <spasmilenkov123@gmail.com>
2023-09-16 21:35:13 +0300

    Migrate the project to typescript add animeJS

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,39 @@
 <script>
-  import WelcomeSection from './components/WelcomeSection.vue';
-  import AboutMe from './components/AboutMe.vue';
-  import Projects from './components/Projects.vue';
-  import Contacts from './components/Contacts.vue';
-  import TechStack from './components/TechStack.vue';
-  export default {
-        components:{
-          WelcomeSection,
-          AboutMe,
-          Projects,
-          TechStack,
-          Contacts
-        }
+import WelcomeSection from './components/WelcomeSection.vue';
+import AboutMe from './components/AboutMe.vue';
+import Projects from './components/Projects.vue';
+import Contacts from './components/Contacts.vue';
+import TechStack from './components/TechStack.vue';
+export default {
+    components: {
+        WelcomeSection,
+        AboutMe,
+        Projects,
+        TechStack,
+        Contacts
     }
+}
 </script>
 
 <template>
-  <main>
-    <WelcomeSection/>
-    <div id="about-me">    
-      <AboutMe/>
-    </div>
-    <div id="stack">
-      <TechStack/>
-    </div>
-    <div id="projects">
-      <Projects/>
-    </div>
-    <div id="contacts">
-      <Contacts/>
-    </div>
-  </main>
+    <main>
+        <WelcomeSection />
+        <div id="about-me">
+            <AboutMe />
+        </div>
+        <div id="stack">
+            <TechStack />
+        </div>
+        <div id="projects">
+            <Projects />
+        </div>
+        <div id="contacts">
+            <Contacts />
+        </div>
+    </main>
 </template>
 <style scoped>
-#projects{
-  min-height: fit-content;
+#projects {
+    min-height: fit-content;
 }
 </style>
