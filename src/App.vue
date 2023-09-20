89442ce7e8c0f5a347258e2b9a875851ce5da2ac
Spas Milenkov <spasmilenkov123@gmail.com>
2023-09-20 22:46:22 +0300

    Add intersection observer for the menu component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,84 @@
+<template>
+    <main>
+        <div id="welcome" class="section">
+            <WelcomeSection />
+        </div>
+        <MenuComponent class="arrow" :active-slot="currentSection" />
+        <div id="about-me" class="section">
+            <AboutMe />
+        </div>
+        <div id="stack" class="section">
+            <TechStack />
+        </div>
+        <div id="projects" class="section">
+            <Projects />
+        </div>
+        <div id="contacts" class="section">
+            <Contacts />
+        </div>
+    </main>
+</template>
+
+
 <script setup lang="ts">
 import WelcomeSection from './components/WelcomeSection.vue';
 import AboutMe from './components/AboutMe.vue';
 import Projects from './components/Projects.vue';
 import Contacts from './components/Contacts.vue';
 import TechStack from './components/TechStack.vue';
+import MenuComponent from './components/MenuComponent.vue';
+import { onMounted, ref } from 'vue'
+
+const currentSection = ref(0)
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            switch (entry.target.id) {
+                case 'welcome':
+                    currentSection.value = 0;
+                    break
+                case 'about-me':
+                    currentSection.value = 1;
+                    console.log(currentSection.value)
+                    break
+                case 'stack':
+                    currentSection.value = 2;
+                    break
+                case 'projects':
+                    currentSection.value = 3;
+                    break
+                case 'contacts':
+                    currentSection.value = 4;
+                    break
+
+            }
+            console.log(currentSection.value)
+        }
+    });
+}, {
+    rootMargin: '-150px',
+});
+onMounted(() => {
+    const sections = [...document.getElementsByClassName('section')];
+    sections.forEach((element) => {
+        observer.observe(element);
+    });
+})
+
+
+
 </script>
 
-<template>
-    <main>
-        <WelcomeSection />
-        <div id="about-me">
-            <AboutMe />
-        </div>
-        <div id="stack">
-            <TechStack />
-        </div>
-        <div id="projects">
-            <Projects />
-        </div>
-        <div id="contacts">
-            <Contacts />
-        </div>
-    </main>
-</template>
+
 <style scoped>
 #projects {
     min-height: fit-content;
+}
+
+.arrow {
+    position: fixed;
+    bottom: 45%;
+    right: 2.5rem;
+    z-index: 999;
 }
 </style>
