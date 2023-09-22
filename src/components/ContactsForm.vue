5d150589b24ab474c73c985fc9a8f69df92774b9
Spas Milenkov <spasmilenkov123@gmail.com>
2023-09-22 17:22:05 +0300

    Add labels, fix text box width

--- src/components/ContactsForm.vue
+++ src/components/ContactsForm.vue
@@ -2,11 +2,29 @@
     <div class="wrapper">
         <form class="form-container" action="https://formsubmit.co/4160c5bc579f39c21948a1db62d1aa90" method="POST">
             <div class="section-wrapper">
-                <input class="text-input" type="text" name="name" placeholder="John Doe" required>
-                <input class="text-input" type="email" name="email" placeholder="johnDoe@example.com" required>
+                <label for="" class="input-label">
+                    Name:
+                    <input class="text-input" type="text" name="name" placeholder="John Doe" required>
+
+                </label>
+                <label for="" class="input-label">
+                    Email:
+                    <input class="text-input" type="email" name="email" placeholder="johnDoe@example.com" required>
+
+                </label>
             </div>
-            <input class="text-input" name="_subject" placeholder="Subject">
-            <textarea class="text-input message" type="text" name="message" placeholder="Message"></textarea>
+
+            <label for="" class="input-label">
+                Subject:
+                <input class="text-input" name="_subject" placeholder="My awesome subject">
+
+            </label>
+            <label for="" class="input-label">
+                Message:
+                <textarea id="message-input" class="text-input message" type="text" name="message"
+                    placeholder="I kind of liked this website, could have been worse!"></textarea>
+
+            </label>
             <input type="hidden" name="_captcha" value="false">
             <input type="hidden" name="_next" value="https://clash2453.github.io/Portfolio-website/#contacts">
             <button class="send-button" type="submit">Send</button>
@@ -44,14 +62,22 @@
     width: fit-content;
     background: var(--accent-color-40);
     color: var(--font-primary);
-    font-size: 1.25rem;
+    font-size: 1.5rem;
     letter-spacing: 0.1rem;
     padding: 0.2rem 0.5rem;
     font-weight: 600;
     border-radius: 1rem;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    align-self: flex-end;
 }
-.text-input{
+
+.section-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+}
+
+.text-input {
     background-color: var(--background-20);
     font-size: 1.25rem;
 
@@ -64,8 +90,18 @@
 
     resize: none;
 
-    
+
 }
+
+#message-input,
+.section-wrapper {
+    width: clamp(18rem, 100%, 40rem);
+}
+
+#message-input {
+    min-height: 15rem;
+}
+
 .text-input::placeholder {
     opacity: 0.5;
 }
