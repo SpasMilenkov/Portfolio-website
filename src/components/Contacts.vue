ff9f3aa53084759658b2d09af7dc427b570255ab
Spas Milenkov <spasmilenkov123@gmail.com>
2023-09-22 17:38:28 +0300

    Add social media section with icons

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -3,10 +3,16 @@
         <div class="form-container">
             <ContactsForm />
         </div>
-        <div class="content">
-            <h1 class="section-header">Contacts</h1>
-            <p class="contacts-text">Saw something that got your attention? Got any suggestions on how to improve this
-                portfolio? Or have a question? Send me an email!</p>
+        <div class="links-container">
+            <h1 class="subtitle">Contact me on social media</h1>
+            <div class="flex">
+                <a href="">
+                    <Icon icon="bi:linkedin" width="1.7rem" />
+                </a>
+                <a href="">
+                    <Icon icon="bi:github" width="1.7rem" />
+                </a>
+            </div>
         </div>
     </section>
 </template>
@@ -21,9 +27,39 @@ import ContactsForm from './ContactsForm.vue';
 <style scoped>
 .container {
     display: flex;
+    align-items: center;
+    justify-content: space-between;
+
     width: 100%;
     min-height: 100vh;
     background: radial-gradient(circle at 5px 5px, var(--accent-color-40) 1px, transparent 0), var(--background);
     background-size: 30px 30px;
+
+    padding: 0 0 0 5vw;
+}
+
+.links-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: var(--g2);
+    width: clamp(20rem, 100%, 37.4rem);
+    height: 100%;
+    min-height: 100vh;
+}
+
+.subtitle {
+    font-size: 2rem;
+    font-weight: 700;
+    letter-spacing: 0.05rem;
+}
+.flex {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    width: 100%;
+    color: var(--accent-color-95);
 }
 </style>
