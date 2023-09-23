cc72966a55b5ec324475461c08f7f4e36a10e38c
Spas Milenkov <spasmilenkov123@gmail.com>
2023-09-23 14:21:08 +0300

    Adjust the menu for the new sections

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -1,48 +1,51 @@
 <template>
     <div class="menu-body">
-        <a :href="'#' + circle" class="circle" v-for="(circle, index) in circles" :class="{ active: activeSlot == index }"
-            :key="index"></a>
+        <a v-for="(circle, index) in circles" @click.prevent.capture ref="indic" :href="'#' + circle"
+            @click="smoothScroll(circle)" class="circle" :class="{ active: activeSlot == index }" :key="index"></a>
     </div>
 </template>
 <script setup lang='ts'>
-
-
 defineProps<{
     activeSlot: number
 }>()
+const circles = ['welcome', 'about-me', 'frontend', 'backend', 'os', 'ver-control', 'design', 'contacts']
+const smoothScroll = (id: string,) => {
+    const element = document.getElementById(id);
+    if (element === null)
+        return
 
-const circles = ['welcome', 'about-me', 'stack', 'contacts']
+    element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
 
-
+}
 </script>
 
-<style scoped>
+<style scoped> 
 .menu-body {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 1rem;
-    gap: 1rem;
-    background-color: var(--background-20);
-    border-radius: 50px;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-}
+     display: flex;
+     flex-direction: column;
+     align-items: center;
+     justify-content: center;
+     padding: 1rem;
+     gap: 1rem;
+     background-color: var(--background-20);
+     border-radius: 50px;
+     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+ }
 
-.circle {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 0.5rem;
-    aspect-ratio: 1;
+ .circle {
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     padding: 0.5rem;
+     aspect-ratio: 1;
 
-    border-radius: 50%;
+     border-radius: 50%;
 
-    background: var(--background-30);
+     background: var(--background-30);
 
-}
+ }
 
-.active {
-    background-color: var(--green-color);
-}
+ .active {
+     background-color: var(--green-color);
+ }
 </style>
